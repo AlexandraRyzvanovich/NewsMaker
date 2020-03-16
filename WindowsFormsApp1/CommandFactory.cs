036852b1e24af54c32cba3564ffcb48ae5db2854
Alexandra Ryzvanovich <Alexandraryzvanovich@gmail.com>
2020-03-16 20:48:46 +0300

    Service init

--- WindowsFormsApp1/CommandFactory.cs
+++ WindowsFormsApp1/CommandFactory.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WindowsFormsApp1.Service;
 
 namespace WindowsFormsApp1
 {
     class CommandFactory
     {
-        public void CallCommand(String command)
+        public void CallCommand(string command)
         {
             switch(command)
             {
-                
+                case "getNewArticle": 
+                    new ArticleCreatorService();
+                    break;
+                default: 
+                   throw new Exception("No such command");
             }
         }
     }
